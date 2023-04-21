b62dce193368d6816cce63982fddd14db31f4349
JoaKox5 <joaquinparraga51@gmail.com>
2023-04-20 23:55:06 -0300

    ej2 guia3

---
+++ Ejercicios/guia3/ej2.py
@@ -0,0 +1,8 @@
+try:
+    numero1 = float(input("Ingresa un numero:  "))
+    numero2 = float(input("Ingresa otro numero:  "))
+        
+    print(numero1 / numero2)
+    
+except ZeroDivisionError:
+    print("No se puede dividir por cero")
